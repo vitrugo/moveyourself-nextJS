68c0496cef10a6381036a828f556321de7195f95
vitrugo <51348273+vitrugo@users.noreply.github.com>
2021-02-25 17:14:18 -0300

    neverstoplearning

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/vitrugo.png" alt="Victor Hugo" />
@@ -8,8 +13,8 @@ export function Profile() {
         <strong>Victor Hugo</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
-          </p>
+          Level {level}
+        </p>
       </div>
     </div>
   );
