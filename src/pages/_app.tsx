f8b856575011f35e33d68e790951a7300fe4fc5d
vitrugo <51348273+vitrugo@users.noreply.github.com>
2021-02-26 17:18:25 -0300

    missioncomplete

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,8 @@
 import '../styles/global.css'
 
-import { ChallengesProvider } from '../contexts/ChallengesContext'
-import React from 'react'
-
 function MyApp({ Component, pageProps }) {
-
-
   return (
-    <ChallengesProvider>
-      <Component {...pageProps} />
-    </ChallengesProvider>
+    <Component {...pageProps} />
   )
 }
 
